8d5d85802d0864bcf21198340a808d6940db13f0
jiyun040 <annajungarmy@gmail.com>
2024-09-08 21:37:53 +0900

    📑 :: [#3] 색상 추가

---
+++ lib/common/const/plyma_colors.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class PlymaColors {
+  static Color white = const Color(0xFFFFFFFF);
+  static Color gray50 = const Color(0xFFFAFAFA);
+  static Color gray100 = const Color(0xFFF4F4F5);
+  static Color gray200 = const Color(0xFFE4E4E7);
+  static Color gray300 = const Color(0xFFD4D4D8);
+  static Color gray400 = const Color(0xFFA1A1AA);
+  static Color gray500 = const Color(0xFF71717A);
+  static Color gray600 = const Color(0xFF52525B);
+  static Color gray700 = const Color(0xFF3F3F46);
+  static Color gray800 = const Color(0xFF27272A);
+  static Color gray900 = const Color(0xFF181818);
+  static Color black = const Color(0xFF000000);
+
+  static Color blue50 = const Color(0xFFF1F9FF);
+  static Color blue100 = const Color(0xFFD9F0FF);
+  static Color blue200 = const Color(0xFFACE3FF);
+  static Color blue300 = const Color(0xFF66CCFF);
+  static Color blue400 = const Color(0xFF52BAFF);
+  static Color blue500 = const Color(0xFF2AAAFF);
+  static Color blue600 = const Color(0xFF008DEC);
+  static Color blue700 = const Color(0xFF0076C4);
+  static Color blue800 = const Color(0xFF005F9E);
+  static Color blue900 = const Color(0xFF00436F);
+  static Color azureBlue = const Color(0xFF007FFF);
+}
