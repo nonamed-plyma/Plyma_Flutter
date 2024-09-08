6d3ec017ee1ac7f60790189b3fbc2d1673910956
jiyun040 <annajungarmy@gmail.com>
2024-09-08 21:48:24 +0900

    ♻️ :: [#3] height, width 파라미터 추가

--- lib/common/component/plyma_button.dart
+++ lib/common/component/plyma_button.dart
@@ -4,25 +4,28 @@ import 'package:plyma/common/const/plyma_colors.dart';
 
 class LoginButton extends StatelessWidget{
   final VoidCallback onTap;
+  final double? width, height;
   final String text;
 
   const LoginButton({
     super.key,
     required this.onTap,
     required this.text,
+    this.width,
+    this.height,
 });
 
   @override
   Widget build (BuildContext context) {
-    final mediaQuery = MediaQuery.of(context);
-    final width = mediaQuery.size.width * 0.8;
-    final height = mediaQuery.size.height * 0.07;
+    // final mediaQuery = MediaQuery.of(context);
+    // final width = mediaQuery.size.width * 0.8;
+    // final height = mediaQuery.size.height * 0.07;
 
     return GestureDetector(
       onTap: onTap,
       child: Container(
-        width: width,
-        height: height,
+        width: width ?? MediaQuery.of(context).size.width,
+        height: height ?? 50,
         alignment: Alignment.center,
         decoration: BoxDecoration(
           color: PlymaColors.azureBlue,
