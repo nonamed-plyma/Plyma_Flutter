11d8b3b9a28de3ab178674ad1cdf8acc483ab179
jiyun040 <annajungarmy@gmail.com>
2024-09-11 22:24:33 +0900

    ♻️ :: [#7] 로고 사이 간격 수정

---
+++ lib/view/login.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:plyma/common/component/plyma_button.dart';
+import 'package:plyma/common/component/plyma_login.dart';
+import 'package:plyma/common/const/plyma_colors.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
+    );
+  }
+}
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: PlymaColors.white,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios),
+          color: PlymaColors.azureBlue,
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+      ),
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: SingleChildScrollView(
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 150),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Image.asset(
+                      'assets/images/Plyma_logo.png',
+                      height: 59,
+                      width: 71,
+                    ),
+                    const SizedBox(height: 30),
+                    const CustomTextField(hinttext: '학번을 입력해주세요 (4글자)'),
+                    const SizedBox(height: 10),
+                    const CustomTextField(hinttext: '비밀번호를 입력하세요 (4~18글자)'),
+                    const SizedBox(height: 50),
+                    LoginButton(onTap: () {}, text: '로그인'),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
