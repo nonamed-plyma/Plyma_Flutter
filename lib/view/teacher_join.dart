5077e123b7a90bcdfd55158a614759a8fbb7cffa
jiyun040 <annajungarmy@gmail.com>
2024-10-13 13:13:36 +0900

    📝 :: [#9] 선생님 회원가입 뷰 수정

--- lib/view/teacher_join.dart
+++ lib/view/teacher_join.dart
@@ -3,10 +3,6 @@ import 'package:plyma/common/component/plyma_button.dart';
 import 'package:plyma/common/component/plyma_login.dart';
 import 'package:plyma/common/const/plyma_colors.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -52,11 +48,11 @@ class TeacherJoinScreen extends StatelessWidget {
                       width: 71,
                     ),
                     const SizedBox(height: 50),
-                    const CustomTextField(hinttext: '이름을 입력해주세요 (1~4자)'),
+                    const CustomTextField(hintText: '이름을 입력해주세요 (1~4자)'),
                     const SizedBox(height: 10),
-                    const CustomTextField(hinttext: '아이디를 입력해주세요(4~10자)'),
+                    const CustomTextField(hintText: '아이디를 입력해주세요(4~10자)'),
                     const SizedBox(height: 10),
-                    const CustomTextField(hinttext: '비밀번호를 입력하세요(1~8자)'),
+                    const CustomTextField(hintText: '비밀번호를 입력하세요(1~8자)'),
                     const SizedBox(height: 40),
                     LoginButton(onTap: () {}, text: '회원가입'),
                   ],
