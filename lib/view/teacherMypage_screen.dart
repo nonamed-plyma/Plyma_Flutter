139dd59f5b1e4e81257f82e7878320306215ee4f
jiyun040 <annajungarmy@gmail.com>
2024-12-04 22:03:26 +0900

    📝 :: [#20] 선생님 마이페이지 컨트롤러 수정

--- lib/view/teacherMypage_screen.dart
+++ lib/view/teacherMypage_screen.dart
@@ -1,17 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:plyma/common/component/studentMypage_heart_view.dart';
-import 'package:plyma/common/component/studentMypage_post_view.dart';
 import 'package:plyma/common/component/teacherMypage_heart_view.dart';
 import 'package:plyma/common/component/teacherMypage_post_view.dart';
 import 'package:plyma/common/const/plyma_colors.dart';
 import 'package:plyma/view/main_screen.dart';
 
-class TeachermypageScreen extends StatelessWidget {
+class TeachermypageScreen extends StatefulWidget {
   const TeachermypageScreen({super.key});
 
   @override
+  State<TeachermypageScreen> createState() => _TeachermypageScreenState();
+}
+
+class _TeachermypageScreenState extends State<TeachermypageScreen> {
+  late PageController pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final PageController pageController = PageController();
     return Scaffold(
       appBar: AppBar(
         backgroundColor: PlymaColors.white,
